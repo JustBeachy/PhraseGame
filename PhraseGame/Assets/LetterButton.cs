2053f03b02d9dfffa445e7c02f2cd82f73cece27
JustBeachy <ajbeach67@gmail.com>
2022-03-27 21:24:05 -0400

    Game logic in (saves not in yet)

--- PhraseGame/Assets/LetterButton.cs
+++ PhraseGame/Assets/LetterButton.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LetterButton : MonoBehaviour
 {
+    public GameObject wordObject;
+    public bool locked = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,22 @@ public class LetterButton : MonoBehaviour
 
     public void OnLetterClick()
     {
+        if (!locked)
+        {
+            gameObject.GetComponent<Button>().interactable = false;
+            wordObject.GetComponent<PhraseList>().LetterClicked(char.Parse(GetComponentInChildren<Text>().text),gameObject);
+        }
+    }
 
+    public void LockLetter()
+    {
+        locked = true;
+        GetComponent<Image>().color = Color.red;
+    }
+
+    public void UnlockLetter()
+    {
+        locked = false;
+        GetComponent<Image>().color = Color.yellow;
     }
 }
